98cfc6e60396abfe4156dfdc249cd9933b561081
Wim Looman <ghostunderscore@gmail.com>
2014-02-09 22:18:39 +1300

    Get generation working with current version and with nonexistent modinfo files for default assets

--- generate.rb
+++ generate.rb
@@ -48,25 +48,27 @@ def mkdirp path
   end
 end
 
-def to_hr struct
-  struct.to_h.tap do |hash|
-    hash.each_pair do |key, value|
-      if value.is_a? OpenStruct
-        hash[key] = to_hr value
-      end
-    end
+def to_hr obj
+  if obj.is_a? OpenStruct
+    Hash[obj.to_h.each_pair.map { |key, value| [ key, to_hr(value) ] }]
+  elsif obj.is_a? Hash
+    Hash[obj.each_pair.map { |key, value| [ key, to_hr(value) ] }]
+  elsif obj.is_a? Array
+    obj.map { |value| to_hr value }
+  else
+    obj
   end
 end
 
 class BaseFile
-  attr_reader :metadata
+  attr_reader :glpg_metadata
   def initialize path, base_path
-    @metadata = OpenStruct.new
-    @metadata.path = path
-    @metadata.root_path = base_path
-    @metadata.relative_path = Pathname.new(path).relative_path_from(Pathname.new(base_path))
-    @metadata.type = File.extname(path)[1..-1]
-    @metadata.name = File.basename(path, ".#{@metadata.type}")
+    @glpg_metadata = OpenStruct.new
+    @glpg_metadata.path = path
+    @glpg_metadata.root_path = base_path
+    @glpg_metadata.relative_path = Pathname.new(path).relative_path_from(Pathname.new(base_path))
+    @glpg_metadata.type = File.extname(path)[1..-1]
+    @glpg_metadata.name = File.basename(path, ".#{@glpg_metadata.type}")
   end
 
   def self.get path, base_path
@@ -85,8 +87,8 @@ class BinaryFile < BaseFile
   end
 
   def save
-    mkdirp(File.dirname metadata.path)
-    File.open(metadata.path, 'wb') do |file|
+    mkdirp(File.dirname glpg_metadata.path)
+    File.open(glpg_metadata.path, 'wb') do |file|
       file.write @data
     end
   end
@@ -98,9 +100,15 @@ class JsonFile < BaseFile
     super
     if File.exists? path
       @data = JSON.parse(File.open(path) { |file| file.read }, symbolize_names: true, object_class: OpenStruct)
+      @exists = true
     else
       @data = OpenStruct.new
+      @exists = false
     end
+  end
+
+  def exists?
+    return @exists
   end
 
   def respond_to_missing? method
@@ -124,8 +132,8 @@ class JsonFile < BaseFile
   end
 
   def save
-    mkdirp(File.dirname metadata.path)
-    File.open(metadata.path, 'w') do |file|
+    mkdirp(File.dirname glpg_metadata.path)
+    File.open(glpg_metadata.path, 'w') do |file|
       file.puts JSON.pretty_generate(self)
     end
   end
@@ -145,25 +153,24 @@ end
 def find_modfile mod_path
   modinfos = Dir["#{mod_path}/*.modinfo"]
   if modinfos.count < 1
-    puts "Error: no .modinfo files found in #{mod_path}"
-    exit 3
-  end
-  if modinfos.count > 1
+    JsonFile.get "#{mod_path}/#{File.basename mod_path}.modinfo", mod_path
+  elsif modinfos.count > 1
     puts "Error: multiple .modinfo files found in #{mod_path}"
     exit 3
+  else
+    JsonFile.get modinfos.first, mod_path
   end
-  JsonFile.get modinfos.first, mod_path
 end
 
 def find_files mod
-  Dir["#{mod.metadata.root_path}/#{mod.path}/**/*"]
+  Dir["#{mod.glpg_metadata.root_path}/#{mod.path}/**/*"]
     .map { |path| File.absolute_path path }
     .select { |path| is_json path }
-    .map { |path| JsonFile.get path, mod.metadata.root_path }
+    .map { |path| JsonFile.get path, mod.glpg_metadata.root_path }
 end
 
 def find_objects mod
-  find_files(mod).select { |file| file.metadata.type == 'object' }
+  find_files(mod).select { |file| file.glpg_metadata.type == 'object' }
 end
 
 def find_plants mod
@@ -171,10 +178,10 @@ def find_plants mod
 end
 
 def generate_frame_overrides frames, override_mod
-  dir = File.join(override_mod.metadata.root_path, File.dirname(frames.metadata.relative_path))
-  downwards = JsonFile.get File.join(dir, "#{frames.metadata.name}-downwards.frames"), override_mod.metadata.root_path
-  leftwards = JsonFile.get File.join(dir, "#{frames.metadata.name}-leftwards.frames"), override_mod.metadata.root_path
-  rightwards = JsonFile.get File.join(dir, "#{frames.metadata.name}-rightwards.frames"), override_mod.metadata.root_path
+  dir = File.join(override_mod.glpg_metadata.root_path, File.dirname(frames.glpg_metadata.relative_path))
+  downwards = JsonFile.get File.join(dir, "#{frames.glpg_metadata.name}-downwards.frames"), override_mod.glpg_metadata.root_path
+  leftwards = JsonFile.get File.join(dir, "#{frames.glpg_metadata.name}-leftwards.frames"), override_mod.glpg_metadata.root_path
+  rightwards = JsonFile.get File.join(dir, "#{frames.glpg_metadata.name}-rightwards.frames"), override_mod.glpg_metadata.root_path
 
   if not downwards.frameGrid
     downwards.frameGrid = OpenStruct.new({
@@ -208,10 +215,10 @@ def generate_frame_overrides frames, override_mod
 end
 
 def generate_image_overrides frames, image, override_mod
-  dir = File.join(override_mod.metadata.root_path, File.dirname(frames.metadata.relative_path))
-  downwards = BinaryFile.get File.join(dir, "#{image.metadata.name}-downwards.#{image.metadata.type}"), override_mod.metadata.root_path
-  leftwards = BinaryFile.get File.join(dir, "#{image.metadata.name}-leftwards.#{image.metadata.type}"), override_mod.metadata.root_path
-  rightwards = BinaryFile.get File.join(dir, "#{image.metadata.name}-rightwards.#{image.metadata.type}"), override_mod.metadata.root_path
+  dir = File.join(override_mod.glpg_metadata.root_path, File.dirname(frames.glpg_metadata.relative_path))
+  downwards = BinaryFile.get File.join(dir, "#{image.glpg_metadata.name}-downwards.#{image.glpg_metadata.type}"), override_mod.glpg_metadata.root_path
+  leftwards = BinaryFile.get File.join(dir, "#{image.glpg_metadata.name}-leftwards.#{image.glpg_metadata.type}"), override_mod.glpg_metadata.root_path
+  rightwards = BinaryFile.get File.join(dir, "#{image.glpg_metadata.name}-rightwards.#{image.glpg_metadata.type}"), override_mod.glpg_metadata.root_path
 
   if not downwards.data
     downwards.data = Magick::Image.from_blob(image.data).first.rotate(180).to_blob
@@ -234,61 +241,55 @@ def generate_image_overrides frames, image, override_mod
 end
 
 def generate_plant_override plant, override_mod, images, image_options
-  file_path = File.join(override_mod.metadata.root_path, plant.metadata.relative_path)
+  file_path = File.join(override_mod.glpg_metadata.root_path, plant.glpg_metadata.relative_path)
 
-  JsonFile.get(file_path, override_mod.metadata.root_path).tap do |object_override|
-    object_override.__merge = []
-    object_override.orientations = {
-      __merge: [
-        "list",
-        [ "update", {
-          anchors: [ "bottom" ]
-        }, {
-          spaces: (0...(plant.metadata.height)).flat_map { |y| (0...(plant.metadata.width)).map { |x| [x, y] } }
-        }],
-        [ "append", {
-          dualImage: "#{Pathname.new(images.downwards.metadata.path).relative_path_from(Pathname.new(File.dirname(file_path)))}:#{image_options}",
-          imagePosition: [0, -((plant.metadata.height - 1) * 8)],
-          frames: 1,
-          animationCycle: 0.5,
-          spaceScan: 0.1,
-          requireTilledAnchors: false,
-          spaces: (0...(plant.metadata.height)).flat_map { |y| (0...(plant.metadata.width)).map { |x| [x, -y] } },
-          anchors: [ "top" ]
-        }],
-        [ "append", {
-          dualImage: "#{Pathname.new(images.leftwards.metadata.path).relative_path_from(Pathname.new(File.dirname(file_path)))}:#{image_options}",
-          imagePosition: [0, 0],
-          frames: 1,
-          animationCycle: 0.5,
-          spaceScan: 0.1,
-          requireTilledAnchors: false,
-          spaces: (0...(plant.metadata.height)).flat_map { |y| (0...(plant.metadata.width)).map { |x| [-y, x] } },
-          fgAnchors: (0...(plant.metadata.width)).map { |val| [1, val] }
-        }],
-        [ "append", {
-          dualImage: "#{Pathname.new(images.rightwards.metadata.path).relative_path_from(Pathname.new(File.dirname(file_path)))}:#{image_options}",
-          imagePosition: [-((plant.metadata.height - 1) * 8), 0],
-          frames: 1,
-          animationCycle: 0.5,
-          spaceScan: 0.1,
-          requireTilledAnchors: false,
-          spaces: (0...(plant.metadata.height)).flat_map { |y| (0...(plant.metadata.width)).map { |x| [y, x] } },
-          fgAnchors: (0...(plant.metadata.width)).map { |val| [-1, val] }
-        }]
-      ]
-    }
+  JsonFile.get(file_path, override_mod.glpg_metadata.root_path).tap do |object_override|
+    object_override.__merge = [
+      [ "overwrite", "orientations" ]
+    ]
+    object_override.orientations = [
+      plant.orientations.first.tap do |top|
+        top[:spaces] = (0...(plant.glpg_metadata.height)).flat_map { |y| (0...(plant.glpg_metadata.width)).map { |x| [x, y] } }
+      end,
+    {
+      dualImage: "#{Pathname.new(images.downwards.glpg_metadata.path).relative_path_from(Pathname.new(File.dirname(file_path)))}:#{image_options}",
+      imagePosition: [0, -((plant.glpg_metadata.height - 1) * 8)],
+      frames: 1,
+      animationCycle: 0.5,
+      spaceScan: 0.1,
+      requireTilledAnchors: false,
+      spaces: (0...(plant.glpg_metadata.height)).flat_map { |y| (0...(plant.glpg_metadata.width)).map { |x| [x, -y] } },
+      anchors: [ "top" ]
+    }, {
+      image: "#{Pathname.new(images.leftwards.glpg_metadata.path).relative_path_from(Pathname.new(File.dirname(file_path)))}:#{image_options}",
+      imagePosition: [-((plant.glpg_metadata.height - 1) * 8), 0],
+      frames: 1,
+      animationCycle: 0.5,
+      spaceScan: 0.1,
+      requireTilledAnchors: false,
+      spaces: (0...(plant.glpg_metadata.height)).flat_map { |y| (0...(plant.glpg_metadata.width)).map { |x| [-y, x] } },
+      fgAnchors: (0...(plant.glpg_metadata.width)).map { |val| [1, val] }
+    }, {
+      image: "#{Pathname.new(images.rightwards.glpg_metadata.path).relative_path_from(Pathname.new(File.dirname(file_path)))}:#{image_options}",
+      imagePosition: [0, 0],
+      frames: 1,
+      animationCycle: 0.5,
+      spaceScan: 0.1,
+      requireTilledAnchors: false,
+      spaces: (0...(plant.glpg_metadata.height)).flat_map { |y| (0...(plant.glpg_metadata.width)).map { |x| [y, x] } },
+      fgAnchors: (0...(plant.glpg_metadata.width)).map { |val| [-1, val] }
+    }]
   end
 end
 
 def generate_plant_overrides plant, override_mod
   image_details = plant.orientations.first.dualImage
   image_path, image_options = image_details.split(':')
-  image = BinaryFile.get File.absolute_path(File.join(File.dirname(plant.metadata.path), image_path)), plant.metadata.root_path
+  image = BinaryFile.get File.absolute_path(File.join(File.dirname(plant.glpg_metadata.path), image_path)), plant.glpg_metadata.root_path
 
-  frames = JsonFile.get File.join(File.dirname(plant.metadata.path), File.basename(image_path, File.extname(image_path)) + ".frames"), plant.metadata.root_path
-  plant.metadata.width = (frames.frameGrid.size[0] / 8.0).ceil
-  plant.metadata.height = (frames.frameGrid.size[1] / 8.0).ceil
+  frames = JsonFile.get File.join(File.dirname(plant.glpg_metadata.path), File.basename(image_path, File.extname(image_path)) + ".frames"), plant.glpg_metadata.root_path
+  plant.glpg_metadata.width = (frames.frameGrid.size[0] / 8.0).ceil
+  plant.glpg_metadata.height = (frames.frameGrid.size[1] / 8.0).ceil
 
   frame_overrides = generate_frame_overrides frames, override_mod
   image_overrides = generate_image_overrides frames, image, override_mod
@@ -297,25 +298,30 @@ def generate_plant_overrides plant, override_mod
 end
 
 def generate_override_modfile mod, override_mod_path
-  override_mod_name = mod.name + '_gravityless_plants'
+  override_mod_name = mod.name ? mod.name + '_gravityless_plants' : 'gravityless_plants'
   JsonFile.get(File.join(override_mod_path, override_mod_name + '.modinfo'), override_mod_path).tap do |override_mod|
     override_mod.name = override_mod_name
-    override_mod.version = mod.version
-    override_mod.dependencies = [ mod.name ]
-    override_mod.path = mod.path
+    override_mod.version = mod.version || 'Beta v. Furious Koala',
+    override_mod.dependencies = [ mod.name ] if mod.exists?
+    override_mod.path = mod.path || '.'
+    override_mod.metadata = OpenStruct.new({
+      version: '0.1',
+      author: 'Nemo157',
+      description: "Gravityless plants for #{mod.name}",
+      support_url: 'https://github.com/Nemo157/gravityless_plants'
+    })
   end
 end
 
 @output_files = []
 puts "Outputting all mods to #{options.output}"
 options.input.map { |mod_path| find_modfile File.absolute_path mod_path }.each do |mod|
-  override_mod_path = File.absolute_path(File.join options.output, "#{File.basename mod.metadata.root_path}_gravityless_plants")
+  override_mod_path = File.absolute_path(File.join(options.output, "#{File.basename mod.glpg_metadata.root_path}_gravityless_plants"))
   rmrf override_mod_path
   override_mod = generate_override_modfile mod, override_mod_path
   @output_files << override_mod
-  puts "Generating #{File.basename override_mod_path}/#{override_mod.name} to override #{File.basename mod.metadata.root_path}/#{mod.name}"
+  puts "Generating #{File.basename override_mod_path}/#{override_mod.name} to override #{File.basename mod.glpg_metadata.root_path}/#{mod.name}"
   find_plants(mod).each do |plant|
-    puts "Found plant in mod #{mod.name} at #{plant.metadata.relative_path}"
     generate_plant_overrides(plant, override_mod).each do |file|
       @output_files << file
     end
@@ -324,10 +330,7 @@ end
 
 @output_files.uniq!
 
-puts ""
-puts "Generated Files"
-puts "==============="
+puts "Generated #{@output_files.count} files"
 @output_files.each do |file|
-  puts "#{file.metadata.path}"
   file.save
 end
