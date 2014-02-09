8d2e6f7339a1b53c158e6ed1c9c32ce57e5a91b9
Wim Looman <ghostunderscore@gmail.com>
2014-02-09 22:38:38 +1300

    Add building zips until modpak's are actually supported

--- build.rb
+++ build.rb
@@ -39,12 +39,21 @@ mkdirp $output_path
 
 system File.join($starbound_bin_dir, 'asset_unpacker'), $default_assets_pak, File.join($temp_path, 'default_assets')
 system 'ruby', File.join(File.dirname(__FILE__), 'generate.rb'), '-i', File.join($temp_path, 'default_assets'), '-o', $temp_path
-File.rename File.join($temp_path, 'default_assets_gravityless_plants', 'gravityless_plants.modinfo'), File.join($temp_path, 'default_assets_gravityless_plants', 'pak.modinfo')
-system File.join($starbound_bin_dir, 'asset_packer'), File.join($temp_path, 'default_assets_gravityless_plants'), File.join($output_path, 'gravityless_plants.modpak')
+rmrf File.join($temp_path, 'gravityless_plants') if File.exists?(File.join($temp_path, 'gravityless_plants'))
+File.rename(File.join($temp_path, 'default_assets_gravityless_plants'), File.join($temp_path, 'gravityless_plants'))
+File.delete File.join($output_path, 'gravityless_plants.zip') if File.exists? File.join($output_path, 'gravityless_plants.zip')
+Dir.chdir $temp_path do
+  system 'zip', '-q', '-r', File.join($output_path, 'gravityless_plants.zip'), 'gravityless_plants'
+end
+File.rename File.join($temp_path, 'gravityless_plants', 'gravityless_plants.modinfo'), File.join($temp_path, 'gravityless_plants', 'pak.modinfo')
+system File.join($starbound_bin_dir, 'asset_packer'), File.join($temp_path, 'gravityless_plants'), File.join($output_path, 'gravityless_plants.modpak')
 
 system 'ruby', File.join(File.dirname(__FILE__), 'generate.rb'), *$mods_to_override.flat_map { |mod| [ '-i', find_mod(mod) ] }, '-o', $temp_path
 $mods_to_override.each do |mod|
+  File.delete File.join($output_path, "#{mod}_gravityless_plants.zip") if File.exists? File.join($output_path, "#{mod}_gravityless_plants.zip")
+  Dir.chdir $temp_path do
+    system 'zip', '-q', '-r', File.join($output_path, "#{mod}_gravityless_plants.zip"), "#{mod}_gravityless_plants"
+  end
   File.rename Dir[File.join($temp_path, "#{mod}_gravityless_plants", '*.modinfo')].first, File.join($temp_path, "#{mod}_gravityless_plants", 'pak.modinfo')
-  #puts File.join($starbound_bin_dir, 'asset_packer'), File.join($temp_path, "#{mod}_gravityless_plants"), File.join($output_path, "#{mod}_gravityless_plants.modpak")
   system File.join($starbound_bin_dir, 'asset_packer'), File.join($temp_path, "#{mod}_gravityless_plants"), File.join($output_path, "#{mod}_gravityless_plants.modpak")
 end
