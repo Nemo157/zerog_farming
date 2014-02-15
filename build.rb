0b32b226b8d38da3e1b98d2902d86d65426499c1
Wim Looman <ghostunderscore@gmail.com>
2014-02-15 16:32:44 +1300

    Add support for old format pak'ed mods and non-installed mods

--- build.rb
+++ build.rb
@@ -2,16 +2,22 @@
 
 require 'bundler/setup'
 
+require 'fileutils'
+require 'pathname'
+
 require_relative 'config'
 
 $starbound_dir = ENV['STARBOUND_DIR'] || File.expand_path('~/Library/Application Support/Steam/SteamApps/common/Starbound')
 $starbound_bin_dir = ENV['STARBOUND_BIN_DIR'] || File.join($starbound_dir, 'Starbound.app/Contents/MacOS')
 
+$extra_mods_dir = File.absolute_path(File.join(File.dirname(__FILE__), 'mods'))
+
 $default_assets_pak = File.join($starbound_dir, 'assets/packed.pak')
 
 $temp_path = File.absolute_path(File.join(File.dirname(__FILE__), 'temp'))
 $output_path = File.absolute_path(File.join(File.dirname(__FILE__), 'output', $config.version))
-$mods_to_override = %w{soy caffeine Starbooze cotton}
+
+$mods_to_override = %w{soy caffeine Starbooze cotton Ore_Farming1_6 BetterMerchants0.07}
 
 def rmrf path
   if File.file? path
@@ -30,11 +36,43 @@ def mkdirp path
 end
 
 def find_mod mod
-  installed_path = File.join($starbound_dir, 'mods', mod)
-  if File.exists? installed_path
-    installed_path
+  paths = [
+    File.join($starbound_dir, 'mods', mod),
+    File.join($extra_mods_dir, mod)
+  ]
+  found = paths.select { |path| File.exists? path }.first
+  if found
+    found
   else
-    raise StandardError, "Cannot find mod #{mod} at #{installed_path}"
+    raise StandardError, "Cannot find mod #{mod} in any of #{paths.map { |path| "'#{path}'"} * ', '}"
+  end
+end
+
+def cp_without_pak src, dst
+  mkdirp dst unless File.exists? dst
+  Dir.foreach(src).reject { |entry| entry == '.' || entry == '..' || entry.end_with?('pak') }.each do |entry|
+    path = File.join(src, entry)
+    if File.file? path
+      FileUtils.cp(path, dst)
+    else
+      cp_without_pak path, File.join(dst, entry)
+    end
+  end
+end
+
+def unpack_if_needed mod
+  if Dir["#{mod}/**/*.pak"].any?
+    unpacked_path = File.join($temp_path, 'inputs', File.basename(mod))
+    rmrf unpacked_path
+    cp_without_pak mod, unpacked_path
+    Dir["#{mod}/**/*.pak"].each do |pak|
+      relative_path = Pathname.new(pak).relative_path_from(Pathname.new(mod))
+      mkdirp File.join(unpacked_path, relative_path)
+      system File.join($starbound_bin_dir, 'asset_unpacker'), pak, File.join(unpacked_path, relative_path)
+    end
+    unpacked_path
+  else
+    mod
   end
 end
 
@@ -50,7 +88,7 @@ end
 File.rename File.join($temp_path, $config.suffix, "#{$config.suffix}.modinfo"), File.join($temp_path, $config.suffix, 'pak.modinfo')
 system File.join($starbound_bin_dir, 'asset_packer'), File.join($temp_path, $config.suffix), File.join($output_path, "#{$config.suffix}.modpak")
 
-system 'ruby', File.join(File.dirname(__FILE__), 'generate.rb'), *$mods_to_override.flat_map { |mod| [ '-i', find_mod(mod) ] }, '-o', $temp_path
+system 'ruby', File.join(File.dirname(__FILE__), 'generate.rb'), *$mods_to_override.flat_map { |mod| [ '-i', unpack_if_needed(find_mod(mod)) ] }, '-o', $temp_path
 $mods_to_override.each do |mod|
   File.delete File.join($output_path, "#{mod}_#{$config.suffix}.zip") if File.exists? File.join($output_path, "#{mod}_#{$config.suffix}.zip")
   Dir.chdir $temp_path do
