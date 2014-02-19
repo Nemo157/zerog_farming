fbba925657fb19d3fe2bf5bcb58e64c3917b1587
Wim Looman <ghostunderscore@gmail.com>
2014-02-19 18:08:24 +1300

    Add release script to push files to github, needs https://github.com/aktau/github-release installed

---
+++ release.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+
+require_relative 'config'
+
+$tag = "v#{$config.version}"
+$name = "#{$config.starbound_version} - Release #{$config.version.split('.').last}"
+$output_path = File.absolute_path(File.join(File.dirname(__FILE__), 'output', $config.version))
+$files = Dir.entries($output_path).reject { |path| path == '.' || path == '..' }.map { |path| File.join($output_path, path) }
+
+$files = $files.reject { |file| file.end_with?('.modpak') }
+
+puts "Tagging release"
+if `git tag`.include? $tag
+  puts "Tag #{$tag} already made"
+else
+  `git tag '#{$tag}'`
+end
+`git push --tags`
+
+puts "Creating release"
+$release_info = `github-release info -r zerog_farming | awk '/^-.*/ { echo = 0 } /^- #{Regexp.quote $tag}/ { echo = 1 } { if (echo == 1) { print } }'`
+if $release_info.empty?
+  `github-release release \
+      --repo zerog_farming \
+      --tag '#{$tag} \
+      --name '#{$name}'`
+else
+  puts "Release #{$name} already exists"
+end
+
+puts "Uploading files"
+$files.each do |file|
+  if $release_info.include? File.basename(file)
+    puts "#{File.basename file} already released for #{$tag}"
+  else
+    `github-release upload \
+        --repo zerog_farming \
+        --tag '#{$tag}' \
+        --name '#{File.basename file}' \
+        --file '#{file}'`
+  end
+end
